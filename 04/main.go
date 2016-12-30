f03f45685b2db6cc1bdffd5104072fc1e203eb3d
Chase Starr <chasestarr@gmail.com>
2016-12-30 09:43:24 -0500

    day 04 part 1, partial complete, getting checksum

---
+++ 04/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
+)
+
+type pair struct {
+	key rune
+	val int
+}
+
+type byVal []pair
+
+func (b byVal) Len() int      { return len(b) }
+func (b byVal) Swap(i, j int) { b[i], b[j] = b[j], b[i] }
+func (b byVal) Less(i, j int) bool {
+	if b[i].val == b[j].val {
+		return b[i].key < b[j].key
+	}
+	return b[i].val > b[j].val
+}
+
+func readFile() []string {
+	input, _ := ioutil.ReadFile("input")
+	return strings.Split(string(input), "\n")
+}
+
+func checkSum(s string) string {
+	memo := make(map[rune]int)
+	for _, c := range s {
+		if _, ok := memo[c]; ok {
+			memo[c] = memo[c] + 1
+		} else {
+			memo[c] = 0
+		}
+	}
+
+	rank := []pair{}
+	for key := range memo {
+		rank = append(rank, pair{key: key, val: memo[key]})
+	}
+
+	sort.Sort(byVal(rank))
+
+	var output string
+	for i := 0; i < 5; i++ {
+		output += string(rank[i].key)
+	}
+
+	return output
+}
+
+func main() {
+	input := readFile()
+
+	for _, line := range input {
+		sections := strings.Split(line, "-")
+		// tail := sections[len(sections)-1]
+		head := sections[:len(sections)-1]
+
+		var room string
+		for _, s := range head {
+			room += s
+		}
+
+		cs := checkSum(room)
+		fmt.Println(cs)
+	}
+}
