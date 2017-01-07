44ae39ac392807994366f689ec5cdb52cb7f1672
Chase Starr <chasestarr@gmail.com>
2017-01-06 22:08:08 -0800

    finished day 07 part 1

--- 07/main.go
+++ 07/main.go
@@ -11,50 +11,110 @@ func readFile() []string {
 	return strings.Split(string(input), "\n")
 }
 
-func getBounds(input string) (int, int) {
-	leftBound := strings.Index(input, "[")
-	rightBound := strings.Index(input, "]")
-	return leftBound, rightBound
-}
+func matchPatternTLS(s string) bool {
+	var isBetweenBrackets bool
+	var output bool
 
-// implementation needs work.. didn't realize line could have > 1 bracket sets
-func cycleForPattern(s string, left, right int) bool {
-	fmt.Println(left, right)
-	fmt.Println(s[left : right-1])
-	for i := left; i < right; i++ {
-		if i >= len(s)-3 {
-			// bail early before trying to access out-of-bound array element
-			return false
+	// bail early before trying to access out-of-bound array element
+outer:
+	for i := 0; i < len(s)-3; i++ {
+		// look ahead at next 4 chars... flip bool
+		for j := i; j < i+4; j++ {
+			if char := rune(s[j]); char == '[' {
+				isBetweenBrackets = true
+				continue outer
+			} else if char == ']' {
+				isBetweenBrackets = false
+				continue outer
+			}
 		}
 
+		// cannot have an "aaaa" pattern
 		if s[i] == s[i+1] {
-			// cannot have an "aaaa" pattern
-			return false
+			continue outer
 		}
 
 		// "abba" --> "ab" == "ab"
-		if s[i]+s[i+1] == s[i+3]+s[i+2] {
-			return true
+		if string(s[i])+string(s[i+1]) == string(s[i+3])+string(s[i+2]) {
+			// if "abba" pattern inBetween, bail and return false
+			if isBetweenBrackets {
+				return false
+			}
+			output = true
 		}
 	}
 
+	return output
+}
+
+func matchPatternSSL(s string) bool {
+	fmt.Println(s)
+	var isBetweenBrackets bool
+	abaPatterns := make(map[string]bool)
+	babPatterns := make(map[string]bool)
+
+outer:
+	for i := 0; i < len(s)-3; i++ {
+
+		// look ahead at next 4 chars...  ignore brackets
+		for j := i; j < i+3; j++ {
+			if char := rune(s[j]); char == '[' {
+				isBetweenBrackets = true
+				continue outer
+			} else if char == ']' {
+				isBetweenBrackets = false
+				continue outer
+			}
+		}
+
+		// cannot have an "aaa" pattern
+		if s[i] == s[i+1] {
+			continue outer
+		}
+
+		// "abc" --> "a" == "c" ?
+		if s[i] == s[i+2] {
+			var pattern string
+			for j := i; j < i+3; j++ {
+				pattern = pattern + string(s[j])
+			}
+
+			opposite := string(s[i+1]) + string(s[i]) + string(s[i+1])
+
+			if isBetweenBrackets {
+				if _, ok := abaPatterns[opposite]; ok {
+					return true
+				}
+				babPatterns[pattern] = true
+			} else {
+				if _, ok := babPatterns[opposite]; ok {
+					return true
+				}
+				abaPatterns[pattern] = true
+			}
+
+			fmt.Println("aba", abaPatterns)
+			fmt.Println("bab", babPatterns)
+			continue outer
+		}
+	}
 	return false
 }
 
 func p1(input []string) {
 	var count int
 	for _, line := range input {
-		leftBound, rightBound := getBounds(line)
-
-		// bail early if "abba" pattern occurs between brackets
-		isBetween := cycleForPattern(line, leftBound+1, rightBound-1)
-		if isBetween {
-			continue
+		if matchPatternTLS(line) {
+			count++
 		}
+	}
+	fmt.Println(count)
+}
 
-		isLeft := cycleForPattern(line, 0, leftBound)
-		isRight := cycleForPattern(line, rightBound+1, len(line)-1)
-		if isLeft || isRight {
+func p2(input []string) {
+	var count int
+	for _, line := range input {
+		if matchPatternSSL(line) {
 			count++
 		}
 	}
@@ -64,4 +124,5 @@ func p1(input []string) {
 func main() {
 	input := readFile()
 	p1(input)
+	p2(input)
 }
